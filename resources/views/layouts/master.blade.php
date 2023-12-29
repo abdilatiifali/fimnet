ed25b674c2af2e76c8f3f7024f38c338a9cbb78d
abdilatiifali <abdilatiifali@gmail.com>
2023-12-29 14:04:25 +0300

    remove alpine js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Fimnet Client Portal</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-	<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="h-full">
 	 @yield('content')
