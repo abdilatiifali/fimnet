ec68c288c2261fd24f3c33a88b6e7dd5bf795f42
abdilatiifali <abdilatiifali@gmail.com>
2023-12-29 22:38:31 +0300

    add favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Fimnet Client Portal</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+  <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
 </head>
 <body class="h-full">
 	 @yield('content')
